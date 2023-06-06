4d41fbd6fc5dc85c3ce2195782cf9b844532cf02
Saul Ramirez <saul.ramirez@disolutionsmx.com>
2023-06-06 13:03:44 -0600

    fixing missin colors

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -6,6 +6,10 @@ class AppColors {
   static const Color backgroundColor = Color(0xFFf5f5f5);
   static const Color darkBackgroundColor = Color(0xFF1a1a1a);
 
+  //App Global Colors:
+  static const Color primaryColor = Color(0xff655b87);
+  static const Color secondaryColor = Color(0xffd9a76c);
+
   // Text
   static const Color bodyColor = Color(0xFF696969);
 
