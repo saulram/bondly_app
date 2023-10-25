8199cad650e25e3d4886fbde39f233a48259f8ea
Saul Islas <saul.ramirez@disolutionsmx.com>
2023-10-24 19:19:33 -0600

    Fixing mentions on mobile

--- lib/features/home/ui/widgets/post_mentions_widget.dart
+++ lib/features/home/ui/widgets/post_mentions_widget.dart
@@ -2,38 +2,57 @@ import 'package:bondly_app/config/colors.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class PostMentionsWidget extends StatelessWidget {
+class PostMentionsWidget extends StatefulWidget {
   final String text;
   final TextStyle? style;
 
   const PostMentionsWidget({super.key, required this.text, this.style});
 
   @override
+  State<PostMentionsWidget> createState() => _PostMentionsWidgetState();
+}
+
+class _PostMentionsWidgetState extends State<PostMentionsWidget> {
+  String text = "";
+  @override
+  void initState() {
+    text = widget.text;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     List<TextSpan> textSpans = [];
 
-    RegExp regex = RegExp(r"\@\[([^\]]+)\]\(([a-f0-9]+)\)");
-    Iterable<Match> matches = regex.allMatches(text);
+    RegExp webRegex = RegExp(r"\@\[([^\]]+)\]\(([a-f0-9]+)\)");
+    RegExp mobileRegex = RegExp(r"\@\[(__)([a-f0-9]+)(__)\]\((__)([^\]]+)(__)\)");
 
     int currentIndex = 0;
 
-    for (Match match in matches) {
-      String userName = match.group(1)!;
-      String userId = match.group(2)!;
+    // Función para procesar las coincidencias
+    void processMatch(Match match, bool isMobileFormat) {
+      String userName, userId;
+
+      if (isMobileFormat) {
+        userId = match.group(2)!;
+        userName = match.group(5)!;
+      } else {
+        userName = match.group(1)!;
+        userId = match.group(2)!;
+      }
 
       // Add the text before the mention
       if (match.start > currentIndex) {
         String beforeMention = text.substring(currentIndex, match.start);
-        textSpans.add(TextSpan(text: beforeMention, style: style));
+        textSpans.add(TextSpan(text: beforeMention, style: widget.style));
       }
 
       // Add the mention
       textSpans.add(TextSpan(
         text: "@$userName",
-        style: style?.copyWith(color: AppColors.secondaryColor),
+        style: widget.style?.copyWith(color: AppColors.secondaryColor),
         recognizer: TapGestureRecognizer()
           ..onTap = () {
-            // Handle the mention tap
             debugPrint("Mention tapped: $userId");
           },
       ));
@@ -41,14 +60,25 @@ class PostMentionsWidget extends StatelessWidget {
       currentIndex = match.end;
     }
 
+    // Procesar las coincidencias del formato web
+    for (Match match in webRegex.allMatches(text)) {
+      processMatch(match, false);
+    }
+
+    // Procesar las coincidencias del formato móvil
+    for (Match match in mobileRegex.allMatches(text)) {
+      processMatch(match, true);
+    }
+
     // Add the remaining text
     if (currentIndex < text.length) {
       String remainingText = text.substring(currentIndex);
-      textSpans.add(TextSpan(text: remainingText, style: style));
+      textSpans.add(TextSpan(text: remainingText, style: widget.style));
     }
 
     return RichText(
       text: TextSpan(children: textSpans),
     );
   }
+
 }
